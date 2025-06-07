6586fb9603351e604202d6236b06eae9cb96eb09
best-wale <bwale974@gmail.com>
2025-06-07 02:50:47 +0100

    Upgrade to my portfolio

---
+++ main.js
@@ -0,0 +1,34 @@
+
+window.addEventListener('load', function () {
+    const elements = document.querySelectorAll('.breath');
+    elements.forEach(element => {
+        element.classList.add('loaded');
+    });
+});
+
+//Dropdown Menu
+function openMenu() {
+    document.getElementById("small-item").style.height = "100vh";
+  
+}
+function closeMenu() {
+    document.getElementById("small-item").style.height = "0%";
+ 
+}
+//
+function click_item(){
+    var menu = document.getElementById("small-item");
+    
+    var menu_icon = document.getElementById("menu")
+   
+    if (menu.style.height == '100vh'){
+        closeMenu();
+        menu_icon.setAttribute('class',' bi bi-text-indent-left')
+        
+        
+
+    }else{
+        openMenu()
+        menu_icon.setAttribute('class','bi bi-x')
+    }
+}
